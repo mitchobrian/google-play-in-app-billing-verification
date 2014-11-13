1c12da922e1f4a4f73764a7a60d0de21ab98f571
Michael Palmer <mail@michaelpalmer.de>
2014-11-13 14:41:56 +0100

    corrected the verfiy example

--- samples/verify.php
+++ samples/verify.php
@@ -10,15 +10,28 @@
 
 set_include_path(get_include_path() . PATH_SEPARATOR . '../library');
 
-require_once 'AndroidMarket/Licensing/ResponseData.php';
-require_once 'AndroidMarket/Licensing/ResponseValidator.php';
+require_once 'library/GooglePlay/InAppBilling/GooglePlayResponseData.php';
+require_once 'library/GooglePlay/InAppBilling/GooglePlayResponseValidator.php';
+require_once 'library/GooglePlay/InAppBilling/GooglePlayInvalidArgumentException.php';
+require_once 'library/GooglePlay/InAppBilling/GooglePlayOrder.php';
+require_once 'library/GooglePlay/InAppBilling/GooglePlayRuntimeException.php';
 
 //Your key, copy and paste from https://market.android.com/publish/editProfile
 define('PUBLIC_KEY', '');
 //Your app's package name, e.g. com.example.yourapp
 define('PACKAGE_NAME', '');
 
-//The | delimited response data from the licensing server
+//The given response from Google Play, should look likes this:
+/* '{
+   "orderId":"12999763169054705758.1371079406387615",
+   "packageName":"com.example.app",
+   "productId":"exampleSku",
+   "purchaseTime":1345678900000,
+   "purchaseState":0,
+   "developerPayload":"bGoa+V7g/yqDXvKRqq+JTFn4uQZbPiQJo4pf9RzJ",
+   "purchaseToken":"rojeslcdyyiapnqcynkjyyjh"
+ }' */
+// http://developer.android.com/google/play/billing/billing_integrate.html
 $responseData = '';
 //The signature provided with the response data (Base64)
 $signature = '';
@@ -32,3 +45,6 @@ $validator = new AndroidMarket_Licensing_ResponseValidator(PUBLIC_KEY, PACKAGE_N
 $valid = $validator->verify($responseData, $signature);
 
 var_dump($valid);
+
+
+$responseData = trim($responseData);
